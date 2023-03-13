a3b8a0d7efdc443920a2d121cf5d5dd9380abb31
Lau G.M <lmartin@animac.com.ar>
2023-03-13 10:09:50 -0300

    mejoras esteticas

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -49,10 +49,10 @@ function evaluarFrase() {
     <main class="container text-white text-lg">
         <h1 class="m-auto max-w-lg p-4">ChatBot de programacion JavaScript hecho por Lau - 2023</h1>
         <div class="m-auto max-w-lg border border-gray-400 p-4 rounded-md bg-gradient-to-r from-sky-500 to-indigo-500">
-            <div id='chat' class="flex flex-col gap-4 h-[450px] overflow-y-auto">
+            <div id='chat' class="flex flex-col gap-4 h-[450px] overflow-y-auto scrollbar">
                 <div v-for="mensaje of mensajes" :key="mensaje.id" class="p-4 max-w-[80%] rounded-3xl text-white" :class="(mensaje.tipo == 'bot')
                     ? 'bg-botColor text-left self-start rounded-bl-none'
-                    : 'bg-userColor text-right self-end rounded-br-none'">
+                    : 'bg-userColor text-right self-end rounded-br-none mr-2'">
                     {{ mensaje.texto }}
                 </div>
             </div>
@@ -65,4 +65,5 @@ function evaluarFrase() {
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
